af124836e6c3ec69ed49b40e29b61eed87fc9568
FuriousGuy1 <m-13767626@moe-dl.edu.my>
2026-02-15 16:53:36 +0800

    Add files via upload

---
+++ fastapi-website-main/app/database.py
@@ -0,0 +1,6 @@
+biography = "Hi, I am Chee Yang and I like doing sports such as swimming, playing badminton, playing pickleball, playing basketball and many more"
+socials = "<a href ='https://www.instagram.com/furios_guy/'>My Instagram</a>"
+
+db = [
+    {"name": "Chee Yang", "hobby": "talking with friends", "age": "12", "color": "black", "biography": biography, "socials": socials},
+]
